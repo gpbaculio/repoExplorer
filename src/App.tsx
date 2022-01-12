36e736f5ba665acfe6f5c453e3acaa474e4becff
gpbaculio <gpbaculio@gmail.com>
2022-01-12 14:48:08 +0800

    Setup Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,133 +1,30 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React, {Suspense} from 'react';
-import {graphql, useLazyLoadQuery} from 'react-relay';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import 'react-native-gesture-handler';
+import React from 'react';
+import {StatusBar} from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {RelayEnvironmentProvider} from 'react-relay';
+import {NavigationContainer} from '@react-navigation/native';
 
 import environment from '../environment';
-import {AppQuery} from './__generated__/AppQuery.graphql';
 
-const RepositoryNameQuery = graphql`
-  query AppQuery {
-    repository(owner: "facebook", name: "relay") {
-      name
-    }
-  }
-`;
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const data = useLazyLoadQuery<AppQuery>(RepositoryNameQuery, {}, {});
-
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {data.repository?.name}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import Navigation from './navigation';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
     <RelayEnvironmentProvider environment={environment}>
-      <Suspense fallback={null}>
-        <SafeAreaView style={backgroundStyle}>
-          <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-          <ScrollView
-            contentInsetAdjustmentBehavior="automatic"
-            style={backgroundStyle}>
-            <Header />
-            <View
-              style={{
-                backgroundColor: isDarkMode ? Colors.black : Colors.white,
-              }}>
-              <Section title="Step One">
-                Edit <Text style={styles.highlight}>App.tsx</Text> to change
-                this screen and then come back to see your edits.
-              </Section>
-              <Section title="See Your Changes">
-                <ReloadInstructions />
-              </Section>
-              <Section title="Debug">
-                <DebugInstructions />
-              </Section>
-              <Section title="Learn More">
-                Read the docs to discover what to do next:
-              </Section>
-              <LearnMoreLinks />
-            </View>
-          </ScrollView>
-        </SafeAreaView>
-      </Suspense>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <StatusBar
+            animated={true}
+            backgroundColor="#61dafb"
+            barStyle="light-content"
+            showHideTransition="slide"
+          />
+          <Navigation />
+        </SafeAreaProvider>
+      </NavigationContainer>
     </RelayEnvironmentProvider>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
