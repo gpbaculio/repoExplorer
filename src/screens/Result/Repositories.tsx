1a2ffeb3d4fb0f81fa274a1e5181354ea6e031de
gpbaculio <gpbaculio@gmail.com>
2022-01-13 15:58:13 +0800

    integrate pagination with flatlist

--- src/screens/Result/Repositories.tsx
+++ src/screens/Result/Repositories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {FlatList, ActivityIndicator} from 'react-native';
 import {graphql, usePaginationFragment} from 'react-relay';
+
 import {DynamicPressable, DynamicText, DynamicView} from '../../components';
 import {Repositories_repositories$key} from '../../__generated__/Repositories_repositories.graphql';
 
@@ -15,7 +16,7 @@ const RepositoriesFragmentGraphQL = graphql`
       @connection(key: "Repositories_repositories_repositories") {
       edges {
         node {
-          nameWithOwner
+          name
           issues(states: OPEN) {
             totalCount
           }
@@ -33,28 +34,80 @@ interface RepositoriesProps {
 }
 
 const Repositories = ({repositoryOwner}: RepositoriesProps) => {
-  const {data, loadNext, refetch, hasNext, isLoadingNext} =
-    usePaginationFragment(RepositoriesFragmentGraphQL, repositoryOwner);
+  const {data, loadNext, hasNext, isLoadingNext} = usePaginationFragment(
+    RepositoriesFragmentGraphQL,
+    repositoryOwner,
+  );
   return (
-    <View>
-      {data?.repositories.edges?.map(edge => {
-        return (
-          <Text key={edge?.node?.nameWithOwner}>
-            {edge?.node?.nameWithOwner}
-          </Text>
-        );
-      })}
-      {hasNext && (
-        <DynamicPressable backgroundColor={'red'} onPress={() => loadNext(10)}>
-          <DynamicText color={'#fff'}>Load More</DynamicText>
-        </DynamicPressable>
-      )}
-      {isLoadingNext && (
-        <DynamicView backgroundColor={'green'}>
-          <DynamicText color={'#fff'}>Loading...</DynamicText>
+    <FlatList
+      data={data?.repositories.edges}
+      contentContainerStyle={{padding: 16}}
+      renderItem={({item}) => (
+        <DynamicView
+          backgroundColor={'#161B22'}
+          marginBottom={8}
+          borderColor={'#30363d'}
+          borderWidth={1}
+          borderRadius={4}
+          flexDirection="row"
+          alignItems="center"
+          key={item?.node?.name}
+          paddingVertical={8}
+          paddingHorizontal={12}>
+          <DynamicText flex={0.65} fontWeight="bold" color={'#fff'}>
+            {item?.node?.name}
+          </DynamicText>
+          <DynamicView flex={0.35}>
+            <DynamicView
+              flex={1}
+              justifyContent="space-between"
+              flexDirection="row">
+              <DynamicText fontSize={12} color={'#fff'}>
+                stars:
+              </DynamicText>
+              <DynamicText fontSize={12} color={'#fff'}>
+                {item?.node?.stargazers.totalCount}
+              </DynamicText>
+            </DynamicView>
+            <DynamicView
+              flex={1}
+              justifyContent="space-between"
+              flexDirection="row">
+              <DynamicText fontSize={12} color={'#fff'}>
+                open issues:
+              </DynamicText>
+              <DynamicText fontSize={12} color={'#fff'}>
+                {item?.node?.issues.totalCount}
+              </DynamicText>
+            </DynamicView>
+          </DynamicView>
         </DynamicView>
       )}
-    </View>
+      keyExtractor={(item, index) => `repo:${index}:${item?.node?.name}`}
+      ListFooterComponent={() => {
+        if (isLoadingNext) {
+          return <ActivityIndicator size="large" color="#868f99" />;
+        }
+        if (hasNext) {
+          return (
+            <DynamicPressable
+              backgroundColor="#007BFF"
+              paddingHorizontal={12}
+              paddingVertical={8}
+              borderRadius={4}
+              alignItems="center"
+              marginVertical={8}
+              disabled={isLoadingNext}
+              onPress={() => loadNext(10)}>
+              <DynamicText fontSize={16} fontWeight="bold" color={'#fff'}>
+                LOAD MORE
+              </DynamicText>
+            </DynamicPressable>
+          );
+        }
+        return null;
+      }}
+    />
   );
 };
 
