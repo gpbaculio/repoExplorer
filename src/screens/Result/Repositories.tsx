a862978c22e20a47cee0464fd49c4e0d391f5f53
gpbaculio <gpbaculio@gmail.com>
2022-01-13 19:33:26 +0800

    add filters on repos pagination

--- src/screens/Result/Repositories.tsx
+++ src/screens/Result/Repositories.tsx
@@ -3,6 +3,7 @@ import {FlatList, ActivityIndicator} from 'react-native';
 import {graphql, usePaginationFragment} from 'react-relay';
 
 import {DynamicPressable, DynamicText, DynamicView} from '../../components';
+import {RepositoriesPaginationQuery} from '../../__generated__/RepositoriesPaginationQuery.graphql';
 import {Repositories_repositories$key} from '../../__generated__/Repositories_repositories.graphql';
 
 const RepositoriesFragmentGraphQL = graphql`
@@ -10,14 +11,16 @@ const RepositoriesFragmentGraphQL = graphql`
   @argumentDefinitions(
     count: {type: "Int", defaultValue: 10}
     cursor: {type: "String", defaultValue: ""}
+    states: {type: "[IssueState!]", defaultValue: [OPEN]}
   )
   @refetchable(queryName: "RepositoriesPaginationQuery") {
     repositories(first: $count, after: $cursor)
       @connection(key: "Repositories_repositories_repositories") {
       edges {
+        cursor
         node {
           name
-          issues(states: OPEN) {
+          issues(states: $states) {
             totalCount
           }
           stargazers {
@@ -25,6 +28,13 @@ const RepositoriesFragmentGraphQL = graphql`
           }
         }
       }
+      pageInfo {
+        startCursor
+        endCursor
+        hasNextPage
+        hasPreviousPage
+      }
+      totalCount
     }
   }
 `;
@@ -34,14 +44,23 @@ interface RepositoriesProps {
 }
 
 const Repositories = ({repositoryOwner}: RepositoriesProps) => {
-  const {data, loadNext, hasNext, isLoadingNext} = usePaginationFragment(
-    RepositoriesFragmentGraphQL,
-    repositoryOwner,
-  );
+  const {data, loadNext, hasNext, isLoadingNext} = usePaginationFragment<
+    RepositoriesPaginationQuery,
+    Repositories_repositories$key
+  >(RepositoriesFragmentGraphQL, repositoryOwner);
+
   return (
     <FlatList
+      ListHeaderComponent={() => (
+        <DynamicView marginBottom={16} alignItems="center">
+          <DynamicText color={'#fff'}>
+            Showing {data?.repositories.edges?.length} of&nbsp;
+            {data?.repositories.totalCount} repositories
+          </DynamicText>
+        </DynamicView>
+      )}
       data={data?.repositories.edges}
-      contentContainerStyle={{padding: 16}}
+      contentContainerStyle={{paddingHorizontal: 16, paddingBottom: 16}}
       renderItem={({item}) => (
         <DynamicView
           backgroundColor={'#161B22'}
@@ -62,28 +81,38 @@ const Repositories = ({repositoryOwner}: RepositoriesProps) => {
               flex={1}
               justifyContent="space-between"
               flexDirection="row">
-              <DynamicText fontSize={12} color={'#fff'}>
-                stars:
-              </DynamicText>
-              <DynamicText fontSize={12} color={'#fff'}>
-                {item?.node?.stargazers.totalCount}
-              </DynamicText>
+              <DynamicView flex={0.5}>
+                <DynamicText marginLeft={'auto'} fontSize={12} color={'#fff'}>
+                  stars:
+                </DynamicText>
+              </DynamicView>
+              <DynamicView flex={0.5}>
+                <DynamicText marginLeft={'auto'} fontSize={12} color={'#fff'}>
+                  {item?.node?.stargazers
+                    ? item?.node?.stargazers.totalCount
+                    : 0}
+                </DynamicText>
+              </DynamicView>
             </DynamicView>
             <DynamicView
               flex={1}
               justifyContent="space-between"
               flexDirection="row">
-              <DynamicText fontSize={12} color={'#fff'}>
-                open issues:
-              </DynamicText>
-              <DynamicText fontSize={12} color={'#fff'}>
-                {item?.node?.issues.totalCount}
-              </DynamicText>
+              <DynamicView flex={0.5}>
+                <DynamicText marginLeft={'auto'} fontSize={12} color={'#fff'}>
+                  issues:
+                </DynamicText>
+              </DynamicView>
+              <DynamicView flex={0.5}>
+                <DynamicText marginLeft={'auto'} fontSize={12} color={'#fff'}>
+                  {item?.node?.issues ? item?.node?.issues.totalCount : 0}
+                </DynamicText>
+              </DynamicView>
             </DynamicView>
           </DynamicView>
         </DynamicView>
       )}
-      keyExtractor={(item, index) => `repo:${index}:${item?.node?.name}`}
+      keyExtractor={(item, index) => `repo:${index}:${item?.cursor}`}
       ListFooterComponent={() => {
         if (isLoadingNext) {
           return <ActivityIndicator size="large" color="#868f99" />;
--- src/screens/Result/Result.tsx
+++ src/screens/Result/Result.tsx
@@ -1,33 +1,52 @@
-import React, {Suspense} from 'react';
+import React, {Suspense, useEffect, useState} from 'react';
 import {ActivityIndicator} from 'react-native';
 import {RouteProp, useRoute} from '@react-navigation/native';
 import {graphql, useLazyLoadQuery} from 'react-relay';
 
 import {AppStackParamList} from '../../navigation/Navigation';
 import Repositories from './Repositories';
-import {DynamicView, ErrorBoundaryWithRetry, ErrorUI} from '../../components';
+import {
+  DynamicPressable,
+  DynamicText,
+  DynamicView,
+  ErrorBoundaryWithRetry,
+  ErrorUI,
+} from '../../components';
 
 import {ResultQuery} from '../../__generated__/ResultQuery.graphql';
+import {IssueState} from '../../__generated__/RepositoriesPaginationQuery.graphql';
+import IssuesFilters from './IssuesFilters';
 
 const ResultQueryGraphQL = graphql`
-  query ResultQuery($login: String!, $count: Int, $cursor: String) {
+  query ResultQuery(
+    $login: String!
+    $count: Int
+    $cursor: String
+    $states: [IssueState!]
+  ) {
     repositoryOwner(login: $login) {
-      ...Repositories_repositories @arguments(count: $count, cursor: $cursor)
+      ...Repositories_repositories
+        @arguments(count: $count, cursor: $cursor, states: $states)
     }
   }
 `;
 
 interface ResultProps {
   fetchKey: number;
+  states: IssueState[];
+  setHasFinishedSuspending: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const Result = ({fetchKey}: ResultProps) => {
+const Result = ({fetchKey, states, setHasFinishedSuspending}: ResultProps) => {
   const {params} = useRoute<RouteProp<AppStackParamList, 'Result'>>();
   const {repositoryOwner} = useLazyLoadQuery<ResultQuery>(
     ResultQueryGraphQL,
-    {login: params.login, count: 10},
+    {login: params.login, count: 10, states},
     {fetchKey},
   );
+  useEffect(() => {
+    setHasFinishedSuspending(true);
+  }, []);
   return (
     <DynamicView flex={1} backgroundColor="#0D1117">
       <Repositories repositoryOwner={repositoryOwner} />
@@ -35,22 +54,39 @@ const Result = ({fetchKey}: ResultProps) => {
   );
 };
 
-export default () => (
-  <ErrorBoundaryWithRetry
-    fallback={({error, retry}) => <ErrorUI error={error} retry={retry} />}>
-    {({fetchKey}) => (
-      <Suspense
-        fallback={
-          <DynamicView
-            flex={1}
-            alignItems="center"
-            justifyContent="center"
-            backgroundColor="#0D1117">
-            <ActivityIndicator size="large" color="#868f99" />
-          </DynamicView>
-        }>
-        <Result fetchKey={fetchKey} />
-      </Suspense>
-    )}
-  </ErrorBoundaryWithRetry>
-);
+export default () => {
+  const [states, setStates] = useState<IssueState[]>(['OPEN', 'CLOSED']);
+  const [hasFinishedSuspending, setHasFinishedSuspending] = useState(false);
+  return (
+    <ErrorBoundaryWithRetry
+      fallback={({error, retry}) => <ErrorUI error={error} retry={retry} />}>
+      {({fetchKey}) => (
+        <DynamicView flex={1} backgroundColor={'#0D1117'}>
+          {hasFinishedSuspending && (
+            <IssuesFilters
+              states={states}
+              setStates={setStates}
+              hasFinishedSuspending={hasFinishedSuspending}
+            />
+          )}
+          <Suspense
+            fallback={
+              <DynamicView
+                flex={1}
+                alignItems="center"
+                justifyContent="center"
+                backgroundColor="#0D1117">
+                <ActivityIndicator size="large" color="#868f99" />
+              </DynamicView>
+            }>
+            <Result
+              fetchKey={fetchKey}
+              states={states}
+              setHasFinishedSuspending={setHasFinishedSuspending}
+            />
+          </Suspense>
+        </DynamicView>
+      )}
+    </ErrorBoundaryWithRetry>
+  );
+};
