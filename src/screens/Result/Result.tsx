a2e66ee3df070e1f25367666c7343458c5adfb83
gpbaculio <gpbaculio@gmail.com>
2022-01-13 16:45:09 +0800

    integrate retry when error occurs

--- src/screens/Result/Result.tsx
+++ src/screens/Result/Result.tsx
@@ -1,13 +1,13 @@
 import React, {Suspense} from 'react';
-import {View, Text, ActivityIndicator} from 'react-native';
+import {ActivityIndicator} from 'react-native';
 import {RouteProp, useRoute} from '@react-navigation/native';
 import {graphql, useLazyLoadQuery} from 'react-relay';
 
 import {AppStackParamList} from '../../navigation/Navigation';
+import Repositories from './Repositories';
+import {DynamicView, ErrorBoundaryWithRetry, ErrorUI} from '../../components';
 
 import {ResultQuery} from '../../__generated__/ResultQuery.graphql';
-import Repositories from './Repositories';
-import {DynamicText, DynamicView} from '../../components';
 
 const ResultQueryGraphQL = graphql`
   query ResultQuery($login: String!, $count: Int, $cursor: String) {
@@ -17,12 +17,16 @@ const ResultQueryGraphQL = graphql`
   }
 `;
 
-const Result = () => {
+interface ResultProps {
+  fetchKey: number;
+}
+
+const Result = ({fetchKey}: ResultProps) => {
   const {params} = useRoute<RouteProp<AppStackParamList, 'Result'>>();
   const {repositoryOwner} = useLazyLoadQuery<ResultQuery>(
     ResultQueryGraphQL,
     {login: params.login, count: 10},
-    {fetchKey: params.fetchKey},
+    {fetchKey},
   );
   return (
     <DynamicView flex={1} backgroundColor="#0D1117">
@@ -32,16 +36,21 @@ const Result = () => {
 };
 
 export default () => (
-  <Suspense
-    fallback={
-      <DynamicView
-        flex={1}
-        alignItems="center"
-        justifyContent="center"
-        backgroundColor="#0D1117">
-        <ActivityIndicator size="large" color="#868f99" />
-      </DynamicView>
-    }>
-    <Result />
-  </Suspense>
+  <ErrorBoundaryWithRetry
+    fallback={({error, retry}) => <ErrorUI error={error} retry={retry} />}>
+    {({fetchKey}) => (
+      <Suspense
+        fallback={
+          <DynamicView
+            flex={1}
+            alignItems="center"
+            justifyContent="center"
+            backgroundColor="#0D1117">
+            <ActivityIndicator size="large" color="#868f99" />
+          </DynamicView>
+        }>
+        <Result fetchKey={fetchKey} />
+      </Suspense>
+    )}
+  </ErrorBoundaryWithRetry>
 );
